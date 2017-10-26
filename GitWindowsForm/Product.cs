582eb3858aeb57ca1687dbf91521d04e311193e5
ШАГ Академія <cuanid236316@gmail.com>
2017-10-26 15:58:45 +0300

    Create tables Product and Category

---
+++ GitWindowsForm/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWindowsForm
+{
+    public class Product
+    {
+        public int IdProduct { get; set; }
+
+        public int IdCategoty { get; set; }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
