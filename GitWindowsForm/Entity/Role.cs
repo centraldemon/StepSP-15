e0fcac60947abeb053029737b986b5066830fec0
mykola <m.savyuk@gmail.com>
2017-10-26 16:08:14 +0300

    Create EFContext

---
+++ GitWindowsForm/Entity/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWindowsForm.Entity
+{
+    public class Role
+    {
+        [ForeignKey("User")]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ GitWindowsForm/Entity/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWindowsForm.Entity
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public int ReleId { get; set; }
+
+        public virtual Role Role { get; set; }
+    }
+}
